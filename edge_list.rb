92f208f4e87b44ed322c4e00b87023b3e2f0b6e4
Erik Trautman <erik.trautman@gmail.com>
2015-07-07 11:09:31 -0700

    Add initial edge list generator

---
+++ edge_list.rb
@@ -0,0 +1,68 @@
+# Creates an Edge List of People
+# Optional: Add Edge Weights
+
+Person = Struct.new(:id, :name, :email)
+
+  Names = [
+    "Harry", "Sally", "Sam", "Michael", "Michelle", "Alok", "Dan", "Nick", "Olga", "Alice", "Joseph", "Donald", "Garrett", "Xin", "Mike", "Adam", "Peter", "Andur", "Tom", "Boris"
+    ]
+
+
+class EdgeList
+
+  def initialize( num_people = 5 )
+    @people = build_people( num_people )
+    @list = build_list
+  end
+
+
+  def build_people( num_people )
+    people = []
+    1.upto( num_people ) do | id |
+      people << self.build_person( id )
+    end
+    people
+  end
+
+
+  def build_person( id )
+    name = Names.sample
+    email = "#{name}#{id}@bar.com"
+    Person.new( id, name, email )
+  end
+
+
+  # Adds a new edge
+  # Weight is the range of possible weights
+  # scarcity is the % odds of no edge
+  def build_list( weight_factor = 10, no_edge_odds = 50 )
+
+    list = []
+    possible_pairs = @people.combination(2)
+
+    possible_pairs.each do |pair|
+
+      # Give a weight unless there shouldn't be an edge at all
+      weight = rand( weight_factor ) * ( 100 / rand( no_edge_odds ) )
+
+      break if weight == 0
+      list << pair + [weight] 
+    end
+
+    list
+  end
+
+
+  def print_list
+    puts "Your Edge List:"
+    puts @list.inspect
+    puts "****************"
+  end
+
+end
+
+
+# Test Script
+
+e = EdgeList.new
+e.print_list
