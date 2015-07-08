5b4dde2be949456c5045b6994879e50c4e7cc5ba
David <davidmeza@outlook.com>
2015-07-08 14:02:12 -0400

    finish adj_matrix class

--- adjacency_matrix.rb
+++ adjacency_matrix.rb
@@ -28,21 +28,36 @@ class AdjacencyMatrix
         @matrix[i][arr[1].id]=arr[2] unless arr[1].id  == i
       end
     end
-    p @matrix
+    #p @matrix
 
   end
 
   def print_matrix
-    # i=0
-    # while i<@matrix.length
-    
+    i=0
+    people = people_list
+
+    people.each do |person|
+      print "    #{person.name}   "
+    end
+    puts
+    while i < @matrix.length
+      print "#{people[i].name}    #{@matrix[i]} \n"
+      i += 1
+    end
+
   end
-  
+
+  def edge_weight(id_person1, id_person2)
+
+    puts @matrix[id_person1][id_person2]
+
+  end
 
 end
 
 matrix = AdjacencyMatrix.new(5)
 matrix.create_matrix
+
 matrix.print_matrix
-puts matrix.people_list
-puts matrix.people_list.length
+
+matrix.edge_weight(1,2)
