56e1549db0ddc1c30633200a631d10ef7ff77d34
alokp <alokpra@gmail.com>
2015-07-08 11:26:35 -0700

    adj matrix complete

--- adj_matrix.rb
+++ adj_matrix.rb
@@ -2,18 +2,22 @@ require_relative "edge_list.rb"
 
 class AdjacencyMatrix
 
-  attr_reader :matrix, :list
+  attr_reader :matrix, :list, :matrix_keys
 
   def initialize (edge_list)
     @list = edge_list.list
   end
 
-
   def print_matrix
-
-    @matrix.each do |line|
-      line.each do |weight|
-        print "#{weight}  "
+    print " ".ljust(10)
+    @matrix_keys.keys.each do |keys|
+      print ("#{@matrix_keys[keys].name}  ").ljust(10)
+    end
+    puts
+    @matrix.each_with_index do |line, indexrow|
+      print (@matrix_keys["#{indexrow}"].name).ljust(10)
+      line.each_with_index do |weight, indexcol|
+        print "#{weight}".ljust(10)
       end
       puts
     end
@@ -23,25 +27,23 @@ class AdjacencyMatrix
   def create_matrix
     @matrix_keys = {}
     simple_list = []
-
+    keys = []
     #Create hash of matrix_keys
     @list.each do |pair|
       (0..1).each do |index|
-        unless (@matrix_keys.keys.to_i).include?(pair[index].id-101)
+        unless keys.include?(pair[index].id-101)
+          keys << pair[index].id-101
           @matrix_keys["#{pair[index].id-101}"] = pair[index]
         end
       end
     end
 
-    puts "Matrix keys"
-    print @matrix_keys.keys
-
     #Create simple_list #[[0, 1, 8],[0, 2, 10], [1, 2, 4]]
     @list.each do |pair|
-      simple_list << [pair[0].id-101, pair[1].id-101, pair[2]]   
+      simple_list << [pair[0].id-101, pair[1].id-101, pair[2]]
     end
 
-    @matrix = Array.new(@matrix_keys.length){Array.new(matrix_keys.length,0)}
+    @matrix = Array.new(@matrix_keys.length){Array.new(@matrix_keys.length,0)}
 
     simple_list.each do |edge| #[0, 1, 8]
       @matrix[edge[0]][edge[1]] = edge[2]
@@ -50,7 +52,9 @@ class AdjacencyMatrix
 
   end
 
-
+  def edge_weight(first_node, last_node)
+    @matrix[first_node-101][last_node-101]
+  end
 end
 
 #each pair ->[struc1, struct2, weight]
@@ -61,9 +65,11 @@ end
 #[#<struct Person id=102, name="Garrett", email="Garrett102@bar.com">, #<struct Person id=103, name="Tom", email="Tom103@bar.com">, 4]]> 
 
 # Test Script
-e = EdgeList.new(3)
+e = EdgeList.new(5)
 am = AdjacencyMatrix.new(e)
 print am.list
 puts
 am.create_matrix
 am.print_matrix
+puts
+puts am.edge_weight(101,102)
