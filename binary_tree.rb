4342a379841384c458098b5b7460734df5786dc4
Kelsey James <kelseybjames@gmail.com>
2016-01-13 10:02:04 -0800

    work on binary tree

--- binary_tree.rb
+++ binary_tree.rb
@@ -3,14 +3,15 @@ class BinaryTree
     @data = data
     @root = BinaryNode.new(nil, nil, @data[0])
     @current_node = @root
+    generate_tree
   end
 
   def add_children(value)
     if value < @current_node.data
       add_left_child(BinaryNode.new(nil, nil, value))
     elsif value > @current_node.data
-
-
+      add_right_child(BinaryNode.new(nil, nil, value))
+    else
     end
 
     @current_node = @root
@@ -21,12 +22,44 @@ class BinaryTree
       @current_node.left = child_node
     else
       @current_node = @current_node.left
-      add_children(child_node)
+      add_children(child_node.data)
     end
   end
 
-  def add_right_child(parent_node, child_node)
+  def add_right_child(child_node)
+    if @current_node.right.nil?
+      @current_node.right = child_node
+    else
+      @current_node = @current_node.right
+      add_children(child_node.data)
+    end
   end
+
+  def generate_tree
+    @data[1..-1].each do |item|
+      add_children(item)
+    end
+  end
+
+  # def render
+  #   puts "#{@current_root.value}".center(40, " ")
+  #   if value < @current_node.data
+  #     print_left_child(BinaryNode.new(nil, nil, value))
+  #   elsif value > @current_node.data
+  #     print_right_child(BinaryNode.new(nil, nil, value))
+  #   else
+  #   end
+
+  #   @current_node = @root
+  # end
+
+  # def print_left_child(parent_node)
+  #   if 
+  # end
+
+  # def print_right_child(parent_node)
+
+  # end
 end
 
 # Set up a root node
@@ -38,3 +71,5 @@ BinaryNode = Struct.new(:left, :right, :data)
 
 
 test_tree = BinaryTree.new([8, 10, 3, 1, 6, 14, 4, 7, 13])
+
+puts "#{test_tree.inspect}"
