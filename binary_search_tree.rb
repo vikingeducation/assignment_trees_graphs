92f836b2a9403b740d5e9f6a8eb6f81b83442da1
Kit Langton <kitlangton@gmail.com>
2016-01-13 16:54:08 -0500

    funk with binary search

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,4 +1,4 @@
-Node = Struct.new(:data, :left, :right)
+Node = Struct.new(:data, :left, :right, :depth)
 
 class BinarySearchTree
   def initialize(array)
@@ -36,16 +36,47 @@ class BinarySearchTree
         node.left = Node.new(data)
       end
     end
+
   end
 
+  def each_depth
+    queue = []
+    @root.depth = 0
+    queue << @root
+    depths = Hash.new { |h,k| h[k] = Array.new }
+    current_depth = 0
+    while node = queue.shift
+      current_depth = node.depth
+      if node.left
+        node.left.depth = current_depth + 1
+        queue << node.left
+      end
+      if node.right
+        node.right.depth = current_depth + 1
+        queue << node.right
+      end
+      depths[node.depth] << node
+    end
+    depths
+  end
+
+  def display_tree
+    each_depth.each do |depth, nodes|
+      print "#{depth}:"
+      nodes.map do |node|
+        print " #{node.data}"
+      end
+      puts
+    end
+  end
 end
 
-given_array = [8, 10, 3, 1, 6, 14, 4, 7, 13].shuffle
+given_array = [8, 10, 3, 1, 6, 14, 4, 7, 13]
 our_tree = BinarySearchTree.new(given_array)
 
 our_tree.make_tree
-p our_tree
-
-puts our_tree
-# >>    8
-# >> #<BinarySearchTree:0x007fc629932f78>
+our_tree.display_tree
+# >> 0: 8
+# >> 1: 3 10
+# >> 2: 1 6 14
+# >> 3: 4 7 13
