4c2774c1943200259e4ad0173ed59ab7e45f3b9e
Andrew Baik <and.baik@gmail.com>
2016-01-13 16:29:49 -0500

    finished adjacency_list

--- adjacency_list.rb
+++ adjacency_list.rb
@@ -44,6 +44,10 @@ class AdjacencyList
       print "\n"
     end
   end
+
+  def edge_weight(id_1, id_2)
+    @id_list[id_1].find_word(@people[id_2]).definition
+  end
 end
 
 # Set up the structure
@@ -53,3 +57,5 @@ el.print_list
 al = AdjacencyList.new( el )
 al.fill_list
 al.print_adj_list
+
+puts al.edge_weight(1,2)
