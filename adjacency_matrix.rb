4e9b24667b6d3116c7fa96ee57d4ed24c3fd6358
cjvirtucio87 <cjv287@gmail.com>
2016-07-14 11:51:52 -0700

    Finished render for adjacency matrix.

--- adjacency_matrix.rb
+++ adjacency_matrix.rb
@@ -23,12 +23,21 @@ class AdjacencyMatrix
     names_variable = array.map do |edge|
       edge[0].name
     end
-    names_variable.uniq.unshift("Bob")
+    names_variable.uniq.unshift("Bob").insert(6,"Aloc")
   end
 
   def print
+    rows = []
+    names = names(@data)
     @state.each_with_index do |row, index|
-      p "#{names(@data)[index].ljust(10)} #{row.map { |cell| cell ? cell.to_s.ljust(3) : " ".ljust(3) }.join('')}"
+      rows << "#{names[index].ljust(10)} #{row.map { |cell| cell ? cell.to_s.ljust(3) : "0".ljust(3) }.join('')}"
+    end
+    max_name_length = names.max{|a,b| a.length <=> b.length}.length
+    justified_names = names.map{|name| name.ljust(max_name_length)}
+    max_row_width = justified_names.length
+    puts justified_names.join('')
+    rows.each do |row|
+      puts row.ljust(max_row_width)
     end
   end
 
