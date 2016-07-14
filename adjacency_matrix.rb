e89fb71a682bf71d73d25bc7eb458a166925703d
Leo Ahnn <leoahnn@gmail.com>
2016-07-14 13:20:07 -0400

    finished adjacency matrix functionality

--- adjacency_matrix.rb
+++ adjacency_matrix.rb
@@ -1,11 +1,35 @@
 require_relative 'edge_list'
+require 'pry'
 
-class AdjacencyMatrix 
-
+class AdjacencyMatrix
+attr_reader :arr
   def initialize(list)
-    @arr = []
+    @arr = Array.new(20) { Array.new(20, nil) }
+    @list = list
+  end
+
+  def build
+    @list.each do |row|
+        @arr[row[0].id][row[1].id] = row[2]
+        # binding.pry
+    end
+  end
+
+  def render
+    @arr.each do |row|
+      row.each do |item|
+        print item.nil? ? "|_|" : item.to_s.center(3)
+      end
+      puts
+    end
   end
 
 end
 
-am = AdjacencyMatrix.new(EDGE_LIST)
+
+
+
+# testing
+a = AdjacencyMatrix.new(EDGE_LIST)
+a.build
+a.render
