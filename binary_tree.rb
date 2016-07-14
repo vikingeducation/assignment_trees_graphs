d4320d43ed2f76aa49f606f1d0c2e05c2a25fd2b
Mike Lee <asackofwheat@gmail.com>
2016-07-14 13:24:49 -0400

    refactoring insert

--- binary_tree.rb
+++ binary_tree.rb
@@ -20,27 +20,34 @@ class BinaryTree
 
   def initialize(array)
     @root = Node.new(nil, nil, array[0])
-    array[1..-1].each {|x| insert(Node.new(nil, nil, x))}
+    @array_length = array.length
+    array[1..-1].each {|x| insert(Node.new(nil, nil, x), @root)}
     # @counter
   end
 
-  def insert(node)
-    current_node = @root
-    loop do
-      if node.data > current_node.data
-        if current_node.right == nil
-          return current_node.right = node
-        else
-          current_node = current_node.right
-          # @counter += 1
-        end
-      elsif node.data < current_node.data
-        if current_node.left == nil
-          return current_node.left = node
-        else
-          current_node = current_node.left
-        end
-      end
+  def insert(node, current_node)
+    # loop do
+    #   if node.data > current_node.data
+    #     if current_node.right == nil
+    #       return current_node.right = node
+    #     else
+    #       current_node = current_node.right
+    #       # @counter += 1
+    #     end
+    #   elsif node.data < current_node.data
+    #     if current_node.left == nil
+    #       return current_node.left = node
+    #     else
+    #       current_node = current_node.left
+    #     end
+    #   end
+    # end
+    if node.data > current_node.data
+      return current_node.right = node unless current_node.right
+      insert(node, current_node.right)
+    elsif node.data < current_node.data
+      return current_node.left = node unless current_node.left
+      insert(node, current_node.left)
     end
   end
 
@@ -48,12 +55,12 @@ class BinaryTree
 
   end
 
-  def breadth_first_search
-    queue = [@root]
-    current_node = @root
-    until queue.empty?
-    end
-  end
+  # def breadth_first_search
+  #   queue = [@root]
+  #   current_node = @root
+  #   until queue.empty?
+  #   end
+  # end
 
 #       current = r 
 #     7    15  
