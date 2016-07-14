22c12746a7efe3d0eac52e4bc5f6d0b4591cee06
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-14 11:30:41 -0700

    finished building matrix

--- graph.rb
+++ graph.rb
@@ -1,20 +1,33 @@
 
 
 require_relative 'edge_list'
+require 'pry'
 
 class AdjacencyMatrix
-  
-  attr_reader :uniq
 
-  def initialize
+  attr_reader :names, :adj_matrix
 
-  @new_array = []
-  EDGE_LIST.each do |item|
-    @new_array << item[0]
-  end
-  @new_array.uniq!
-  @new_array.each do |item|
-    print item.name.center(10)
+  def initialize(array)
+    @lookup_hash = {}
+    matrix_size = matrix_sizer(array)
+    @adj_matrix = Array.new(matrix_size){ Array.new(matrix_size, nil)}
+    build_matrix(array)
   end
 
+  def matrix_sizer(array)
+    array.each do |item|
+        @lookup_hash[item[0].id] = item[0].name
+        @lookup_hash[item[1].id] = item[1].name
+    end
+    @lookup_hash.keys.count
+  end
+
+  def build_matrix(array)
+    array.each do |ele|
+      @adj_matrix[ele[0].id][ele[1].id] = ele[2]
+    end
+  end
 end
+
+am = AdjacencyMatrix.new(EDGE_LIST)
+p am
