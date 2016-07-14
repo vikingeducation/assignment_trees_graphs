d34168939e94c0c80342bc1dd1b1d3af4e8fda1d
Alex Lach <alexglach@gmail.com>
2016-07-14 13:41:55 -0400

    starting on graphs

--- graph.rb
+++ graph.rb
@@ -1,8 +1,20 @@
-Person = Struct.new(:id, :name, :email)
 
-require 'edge_list'
 
-class Graph
+require_relative 'edge_list'
 
-puts EDGE_LIST
+class AdjacencyMatrix
+  
+  attr_reader :uniq
+
+  def initialize
+
+  @new_array = []
+  EDGE_LIST.each do |item|
+    @new_array << item[0]
+  end
+  @new_array.uniq!
+  @new_array.each do |item|
+    print item.name.center(10)
+  end
+
 end
