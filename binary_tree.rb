c6436a3a52ec60ed29e4f1801591595ad58a1f8a
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-14 12:00:22 -0500

    conditional logic to node builder and created a tree

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,36 +1,72 @@
 
-node = Struct.new(:left, :right, :data)
+Node = Struct.new(:left, :right, :data)
 
 class BinaryTree
 
   def initialize(array)
+    @array = array
     @root = node.new(nil, nil, array[0])
     @node = nil
+    @holder = 10
+    @direction = nil
   end
 
-  def add_node
 
+
+
+  def add_node
+    while @root
+      if @node.data > @root.data
+        @holder = @root
+        @root = @root.right
+        @direction = right
+      else
+        holder = @root
+        @root = @root.left
+        @direction = left
+      end
+
+    end
+
+  end
+
+
+       
 
   end
 
   def build_tree
-
+    @array.each do |num| 
+      node = Node.new(nil,nil, num)
+      add_node(node)
+      @node = node
+    end
 
   end
-
+ 
 
 end
+
+#[1,2,3,4,5,6,7]
+
+  #
+
+
+
+
+
+
 
 # initialize with root node as array[0]
 # array#sort
 # array.each do |node|
-  add_node(node)
-    if node > root
-      if !!node
-        node.right = node
-    else
-      node.left = node
-    end
-    @root = node
-end
+#   add_node(node)
+#     if node > root
+#       if !!node
+#         node.right = node
+#     else
+#       node.left = node
+#     end
+#     @root = node
+# end
 
