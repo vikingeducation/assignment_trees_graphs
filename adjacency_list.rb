8a19851bc2e8c696cf08701c62d25085870c15de
Hannah Squier <hannahsquier@gmail.com>
2016-07-14 13:51:56 -0700

    create adjacency list populate list method

--- adjacency_list.rb
+++ adjacency_list.rb
@@ -1,9 +1,11 @@
 #[LL,LL]
-require 'edge_list'
+require_relative 'edge_list'
 
 Node = Struct.new(:id, :name, :next_node)
 
 class AdjacencyList
+  attr_reader :array
+
   def initialize(edge_list)
     @edge_list = edge_list
     @array = Array.new(20) { nil }
@@ -12,24 +14,32 @@ class AdjacencyList
 
 
   def populate_list
+    previous_node = nil
+    
     @edge_list.each do |edge|
       origin = edge[0]
       new_person = edge[1]
-      new_node = create_node(first_person)
-      current_node = new_node
+      new_node = create_node(new_person)
       
-      begin 
-        if array[origin.id].nil?    
+      current_node = new_node
+
+      loop do
+        if array[origin.id].nil?
+          previous_node = current_node
           @array[origin.id] = new_node if current_node == new_node
-
+          break
+        elsif current_node.next_node.nil?
+          previous_node.next_node = new_node
+          previous_node = current_node
+          break
         else
-          node = 
+          current_node = current_node.next_node
         end
-      end until current_node.next_node.nil?
-
-
+      end 
 
     end
+
+    @array
   end
 
   def create_node(person)
@@ -37,3 +47,5 @@ class AdjacencyList
   end
 
 end
+
+p AdjacencyList.new(EDGE_LIST).array
