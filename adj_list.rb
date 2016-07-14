997f0a99f4e7fe133fac526104663104bf7faf1f
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-14 11:37:33 -0700

    What about Bob?

--- adj_list.rb
+++ adj_list.rb
@@ -1,28 +1,6 @@
 require_relative './edge_list'
 require_relative './linked_list'
 
-NAMES = {
-  0 => 'Bob',
-  1 => 'Harry',
-  2 =>  'Sally',
-  3 =>  'Sam',
-  4 => 'Michael',
-  5 => 'Michelle',
-  6 => 'Alok',
-  7 => 'Dan',
-  8 => 'Nick',
-  9 => 'Olga',
-  10 => 'Alice',
-  11 => 'Donald',
-  12 => 'Garrett',
-  13 => 'Xin',
-  14 => 'Mike',
-  15 => 'Adam',
-  16 => 'Peter',
-  17 => 'Andur',
-  18 => 'Tom',
-  19 => 'Boris'
-}
 
 class AdjacencyList
   attr_reader :origins
