a610d443ecab5c8f3c6e182e9aa205b3c7529dd6
“Matthew <matthew.hinea@gmail.com>
2016-07-14 13:17:49 -0400

    testing out a recursive build function

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -5,7 +5,14 @@ class BinarySearchTree
 
   def initialize(array)
     @root = build_node(array[0])
-    # order array here
+    array.shift
+
+  end
+
+  def build_tree(array)
+    array.each do |item|
+      add_node(item)
+    end
   end
 
   def build_node(n)
@@ -28,6 +35,21 @@ class BinarySearchTree
           return current_lvl.left = build_node(n)
         end
       end
+    end
+  end
+
+  def recursive_build_tree(array)
+    array.each do |item|
+      recursive_add_node(build_node(item))
+    end
+  end
+
+  def recursive_add_node(node, current_lvl = @root)
+    return node unless current_lvl
+    if node.data > current_lvl.data
+      current_lvl.right = recursive_add_node(node, current_lvl.right)
+    else
+      current_lvl.left = recursive_add_node(node, current_lvl.left)    
     end
   end
 
--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -1,8 +1,7 @@
 require "binary_search_tree"
 
 describe BinarySearchTree do
-  let(:tree){BinarySearchTree.new([8, 10, 3, 1, 6, 14,
-                                            4, 7, 13])}
+  let(:tree){BinarySearchTree.new([8])}
   describe "#build_node" do
     it "returns a node object" do
       expect(tree.build_node(1)).to be_a(Node)
@@ -27,5 +26,34 @@ describe BinarySearchTree do
       tree.add_node(11)
       expect(tree.root.right.data).to eq(11)
     end
+
+    it "it successfully updates child pointers" do
+      tree.add_node(11)
+      tree.add_node(12)
+      expect(tree.root.right.right.data).to eq(12)
+    end
   end
+
+  describe "#build_tree" do 
+    it "builds the tree" do 
+      tree.build_tree([10, 3, 1, 6, 14, 4, 7, 13])
+      expect(tree.root.right.data).to eq(10)
+      expect(tree.root.right.right.data).to eq(14)
+      expect(tree.root.left.data).to eq(3)
+      expect(tree.root.left.left.data).to eq(1)
+      expect(tree.root.left.right.data).to eq(6)
+    end
+  end
+
+  describe "#recursive_tree_build" do 
+    it "builds the tree recursively" do 
+      tree.recursive_build_tree([10, 3, 1, 6, 14, 4, 7, 13])
+      # expect(tree.root.right.data).to eq(10)
+      expect(tree.root.right.right.data).to eq(14)
+      expect(tree.root.left.data).to eq(3)
+      expect(tree.root.left.left.data).to eq(1)
+      expect(tree.root.left.right.data).to eq(6)
+    end
+
+  end 
 end
