1a6bf89cd4f71c507b4031f8ed5da909d57cc253
Adrian Mui <adrian.mui@gmail.com>
2016-07-14 12:41:18 -0400

    binary_tree.rb fin

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,16 +1,16 @@
-require 'pry'
-
+#binary_tree.rb
 Node = Struct.new :val, :left, :right
 
 class BinaryTree
-  attr_reader :arr
+  attr_reader :arr, :root
   def initialize(arr)
     @root = Node.new(arr[0], nil, nil)
     @arr = arr
+    build
   end
 
-  def build(arr)
-    arr.each do |val|
+  def build
+    @arr.each do |val|
       holder = @root
       next if val == @root.val
       # creates a new node using array value
@@ -19,7 +19,7 @@ class BinaryTree
       loop do
         if node.val < holder.val
           if holder.left.nil?
-            holder.left(node)
+            holder.left = node
             break
           else
             holder = holder.left
@@ -27,7 +27,7 @@ class BinaryTree
         # checks right
         elsif node.val > holder.val
           if holder.right.nil?
-            holder.right(node)
+            holder.right = node
             break
           else
             holder = holder.right
@@ -38,4 +38,13 @@ class BinaryTree
   end
 end
 
-binary_tree = BinaryTree.new([8, 10, 3, 1, 6, 14, 4, 7, 13])
+#testing the process
+# b = BinaryTree.new([8, 10, 3, 1, 6, 14, 4, 7, 13])
+# p b.arr
+# p "#{b.root.val}"
+# p "#{b.root.left.val}  #{b.root.right.val}"
+# p "#{b.root.left.left.val}  #{b.root.left.right.val}  #{b.root.right.right.val}"
+
+
+
+
