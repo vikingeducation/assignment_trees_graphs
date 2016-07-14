ae31a868f3ee4f6423ab77c1250b577eacb2a05a
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-14 11:36:12 -0700

    Add print list method

--- adj_list.rb
+++ adj_list.rb
@@ -2,6 +2,7 @@ require_relative './edge_list'
 require_relative './linked_list'
 
 NAMES = {
+  0 => 'Bob',
   1 => 'Harry',
   2 =>  'Sally',
   3 =>  'Sam',
@@ -20,23 +21,44 @@ NAMES = {
   16 => 'Peter',
   17 => 'Andur',
   18 => 'Tom',
-  19 => 'Boris',
+  19 => 'Boris'
 }
 
 class AdjacencyList
+  attr_reader :origins
 
   def initialize(list)
     @list = list
-    @origins = Array.new(19)
+    @origins = Array.new(20)
+  end
+
+  def convert_list
+    @list.each do |connection|
+      insert_connection(connection)
+    end
   end
 
   def insert_connection(connection)
-    @origins[connection[0].id - 1] ||= LinkedList.new
-    @origins[connection[0].id - 1].add_node(connection[1].id - 1, connection[2])
+    @origins[connection[0].id ] ||= LinkedList.new
+    @origins[connection[0].id].add_node(connection[1].id, connection[2])
   end
 
-
+  def print_adj_list
+    @origins.each_with_index do |list, index|
+      print "#{ NAMES[ index ] }: "
+      unless list.nil?
+        current_node = list.first
+        until current_node.nil?
+          print "#{NAMES[current_node.id]}(#{current_node.weight}) "
+          current_node = current_node.next_node
+        end
+      end
+      puts
+    end
+  end
 
 end
 
 a = AdjacencyList.new(EDGE_LIST)
+a.convert_list
+a.print_adj_list
--- adj_matrix.rb
+++ adj_matrix.rb
@@ -1,25 +1,4 @@
 require_relative './edge_list'
-NAMES = {
-  1 => 'Harry',
-  2 =>  'Sally',
-  3 =>  'Sam',
-  4 => 'Michael',
-  5 => 'Michelle',
-  6 => 'Alok',
-  7 => 'Dan',
-  8 => 'Nick',
-  9 => 'Olga',
-  10 => 'Alice',
-  11 => 'Donald',
-  12 => 'Garrett',
-  13 => 'Xin',
-  14 => 'Mike',
-  15 => 'Adam',
-  16 => 'Peter',
-  17 => 'Andur',
-  18 => 'Tom',
-  19 => 'Boris',
-}
 
 class AdjacencyMatrix
   attr_reader :list
@@ -31,7 +10,7 @@ class AdjacencyMatrix
   def convert_edge_list
     @edj_matrix = Array.new(19){Array.new(19)}
     @list.each do |connection|
-      @edj_matrix[connection[0].id - 1][connection[1].id - 1] = connection[2]
+      @edj_matrix[connection[0].id][connection[1].id] = connection[2]
       #@edj_matrix[connection[1].id - 1][connection[0].id - 1] = connection[2]
     end
     @edj_matrix
@@ -45,12 +24,12 @@ class AdjacencyMatrix
     puts "ORIGIN".center(width * @edj_matrix.size)
     @edj_matrix.size.times do |col|
       print "DEST".center(width) if col == 0
-      print "#{NAMES[col + 1]}".center(width)
+      print "#{NAMES[col]}".center(width)
     end
     puts
 
     @edj_matrix.size.times do |row|
-      print "#{NAMES[row + 1]}".center(width)
+      print "#{NAMES[row]}".center(width)
       @edj_matrix.size.times do |col|
         if @edj_matrix[col][row].nil?
           print "X".center(width)
@@ -65,7 +44,7 @@ class AdjacencyMatrix
 
 
   def edge_weight(origin, destination)
-    @edj_matrix[origin - 1][destination - 1]
+    @edj_matrix[origin][destination]
   end
 end
 
