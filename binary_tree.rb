3807c80fc126708cbb37ca5a18c7fa3d2f4ace3c
Alex Lach <alexglach@gmail.com>
2016-07-14 12:54:40 -0400

    tree built

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,25 +1,43 @@
 Node = Struct.new( :data, :left, :right )
 
+
 class BinaryTree
+
+  attr_reader :input_arr, :root
 
   def initialize(arr)
     @input_arr = arr
-    @root = Node.new( data: @input_arr.shift )
+    @root = Node.new( @input_arr.shift )
+    build_tree
   end
 
   def add_node( arr, root )
-    node = Node.new( data: arr.shift )
+    node = Node.new( arr.shift )
     node.data < root.data ? root.left = node : root.right = node
   end
 
-  def find_leaf( arr, root )
-    return root if root.data.is_nil?
-    root.data > element ? find_leaf(arr, root.left) : find_leaf(root.right)
-
+  def find_leaf( element, root )
     if root.data > element
       return root if root.left == nil
       find_leaf(element, root.left)
-    elsif
+    else
       return root if root.right == nil
+      find_leaf(element, root.right)
+    end
   end
+
+  def build_tree
+    
+    until @input_arr.empty?
+      add_node(@input_arr, find_leaf(@input_arr[0], @root))
+    end
+
+  end
+
+
+
 end
+
+b = BinaryTree.new([8, 10, 3, 1, 6, 14, 4, 7, 13])
+puts b.root.right.right.left.data
+
