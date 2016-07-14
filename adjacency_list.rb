341a997ac34788405e3d7ae509ffe896205fab70
schoetlr <srlrs20020@gmail.com>
2016-07-14 16:26:06 -0400

    working on populating list

--- adjacency_list.rb
+++ adjacency_list.rb
@@ -14,7 +14,26 @@ class AdjacencyList
   def populate_list
     @edge_list.each do |edge|
       origin = edge[0]
-      dest = edge[1]
+      new_person = edge[1]
+      new_node = create_node(first_person)
+      current_node = new_node
+      
+      begin 
+        if array[origin.id].nil?    
+          @array[origin.id] = new_node if current_node == new_node
+
+        else
+          node = 
+        end
+      end until current_node.next_node.nil?
+
+
+
     end
   end
+
+  def create_node(person)
+    Node.new(person.id, person.name, nil)
+  end
+
 end
