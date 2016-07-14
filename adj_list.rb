0899b0c739aef027e108f93a8fbba61800cb7b9c
Alex Lach <alexglach@gmail.com>
2016-07-14 17:10:33 -0400

    done with final questions, need work on adj list

--- adj_list.rb
+++ adj_list.rb
@@ -4,32 +4,38 @@ require 'pry'
 
 class AdjacencyList
 
+  attr_reader :adj_list
+
   def initialize(array)
     @adj_list = []
     @nodes = []
     array_size = array_sizer(array)
-
+    build_list
+    populate(array)
   end
 
   def build_list
     @nodes.each do |node|
-      @adj_list << LinkedList.new(node)
+      @adj_list << DataStructuresAssignment::LinkedList.new(node)
     end
   end
 
   def array_sizer(array)
     array.each do |item|
-        @nodes += item[0]
-        @nodes += item[1]
+        @nodes << item[0]
+        @nodes << item[1]
     end
-    @nodes.uniq!.sort!
+    @nodes.id.uniq!.sort!
     @nodes.size
   end
 
   def populate(array)
     array.each do |row|
-      @adj_list[row[0].id].insert([row[1], row[2]], :tail)
+      (@adj_list[row[0].id]).insert([row[1], row[2]], :tail)
     end
   end
 
 end
+
+a = AdjacencyList.new(EDGE_LIST)
+a.adj_list
