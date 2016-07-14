94b09e378928e4e0fc3ac20b8331e5ae493ba9c8
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-14 12:58:48 -0500

    finished tree

--- binary_tree.rb
+++ binary_tree.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 Node = Struct.new(:left, :right, :data)
 
+
 class BinaryTree
   attr_accessor :root
   def initialize(array)
@@ -11,29 +12,31 @@ class BinaryTree
     @holder = nil
     @top = @root
     @direction = nil
+    @tree = []
   end
 
 
-  def add_node(node)
-    while @top  
-      if node.data > @top.data
-        @holder = @top 
-        @top = @top.right
-        @direction = "right"
+  def add_node(parent_node, child_node)
+    if child_node.data > parent_node.data
+      if parent_node.right
+        add_node(parent_node.right, child_node)
       else
-        @holder = @top
-        @top = @top.left
-        @direction = 'left'
+        parent_node.right = child_node
+      end
+    else
+      if parent_node.left
+        add_node(parent_node.left, child_node)
+      else
+        parent_node.left = child_node
       end
     end
-    @direction == "left" ? @holder.left = node : @holder.right = node
   end
 
 
   def build_tree
     @array.each do |num|
       node = Node.new(nil,nil, num)
-      add_node(node)
+      add_node(@root, node)
       # @node = node
     end
 
@@ -41,40 +44,10 @@ class BinaryTree
 
   def display_tree
     p @root
-    p @holder
-    p @top
-    p @direction
-
-
   end
-
 
 end
 
 binary_tree = BinaryTree.new([8, 10, 3, 1, 6, 14, 4, 7, 13])
 binary_tree.build_tree
 binary_tree.display_tree
-
-
-#[1,2,3,4,5,6,7]
-
-#
-
-
-
-
-
-
-
-# initialize with root node as array[0]
-# array#sort
-# array.each do |node|
-#   add_node(node)
-#     if node > root
-#       if !!node
-#         node.right = node
-#     else
-#       node.left = node
-#     end
-#     @root = node
-# end
