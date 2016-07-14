9496f972ed54757ddaa7f46f4edf068b3b7e65de
Vishal Lama <vishal.lama@gmail.com>
2016-07-14 11:14:03 -0600

    Finish implementation of BinarySearchTree

--- binary_search_tree/bst.rb
+++ binary_search_tree/bst.rb
@@ -3,19 +3,28 @@
 Node = Struct.new(:left, :right, :data)
 
 class BST
+  attr_reader :root
 
   def initialize(arr = [])
-    @root = Node.new(nil,nil,nil)
-    arr.each { |value| insert(@root, Node.new(nil, nil, value)) }
+    @root = Node.new(nil, nil, arr[0])
+    arr[1..-1].each { |value| insert(@root, Node.new(nil, nil, value)) }
   end
 
   def insert(root_node, new_node)
-    root_node = new_node if root_node.data == nil
+    root_node = new_node if root_node.nil?
 
     if new_node.data < root_node.data
-      insert(root_node.left, new_node)
+      if root_node.left.nil?
+        root_node.left = new_node
+      else
+        insert(root_node.left, new_node)
+      end
     elsif new_node.data > root_node.data
-      insert(root_node.right, new_node)
+      if root_node.right.nil?
+        root_node.right = new_node
+      else
+        insert(root_node.right, new_node)
+      end
     else
       return
     end
@@ -23,16 +32,10 @@ class BST
 
   private
 
-  def nil?
-
-  end
-
 end
 
-
-
 # tests
-bst=BST.new([1])
-#bst = BST.new([1, 2, 3, 4, 5])
 
+bst = BST.new([5, 4, 1, 6, 3])
 p bst
+
