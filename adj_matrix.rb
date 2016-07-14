91785f6b01c6e92db7541759149e45ae2c302f1d
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-14 10:46:23 -0700

    Add print_matrix method

--- adj_matrix.rb
+++ adj_matrix.rb
@@ -1,5 +1,25 @@
 require_relative './edge_list'
-
+NAMES = {
+  1 => 'Harry',
+  2 =>  'Sally',
+  3 =>  'Sam',
+  4 => 'Michael',
+  5 => 'Michelle',
+  6 => 'Alok',
+  7 => 'Dan',
+  8 => 'Nick',
+  9 => 'Olga',
+  10 => 'Alice',
+  11 => 'Donald',
+  12 => 'Garrett',
+  13 => 'Xin',
+  14 => 'Mike',
+  15 => 'Adam',
+  16 => 'Peter',
+  17 => 'Andur',
+  18 => 'Tom',
+  19 => 'Boris',
+}
 
 class AdjacencyMatrix
   attr_reader :list
@@ -9,15 +29,38 @@ class AdjacencyMatrix
   end
 
   def convert_edge_list
-    edj_matrix = Array.new(19){Array.new(19)}
+    @edj_matrix = Array.new(19){Array.new(19)}
     @list.each do |connection|
-      edj_matrix[connection[0].id - 1][connection[1].id - 1] = connection[2]
+      @edj_matrix[connection[0].id - 1][connection[1].id - 1] = connection[2]
+      #@edj_matrix[connection[1].id - 1][connection[0].id - 1] = connection[2]
     end
-    edj_matrix
+    @edj_matrix
   end
 
   def print_matrix
-    p convert_edge_list
+    width = 8
+    convert_edge_list
+
+    puts
+    puts "ORIGIN".center(width * @edj_matrix.size)
+    @edj_matrix.size.times do |col|
+      print "DEST".center(width) if col == 0
+      print "#{NAMES[col + 1]}".center(width)
+    end
+    puts
+
+    @edj_matrix.size.times do |row|
+      print "#{NAMES[row + 1]}".center(width)
+      @edj_matrix.size.times do |col|
+        if @edj_matrix[col][row].nil?
+          print "X".center(width)
+        else
+          print " #{ @edj_matrix[col][row] } ".center(width)
+        end
+      end
+      puts
+    end
+
   end
 end
 
