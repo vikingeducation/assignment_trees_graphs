691c37383d955a4dbf3b53093346ee7b46327afe
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-09 13:56:19 -0500

    Adjecency matrix!

--- lib/adjacency_matrix.rb
+++ lib/adjacency_matrix.rb
@@ -1,13 +1,30 @@
-
+require_relative "edge_list"
 class AdjacencyMatrix
 
-  def initialize(edge_list)
+  def initialize(edge_list = EDGE_LIST)
     @edge_list = edge_list
-    generate_matrix(edge_list)
+    @matrix = generate_matrix
   end
 
   def generate_matrix
     empty_matrix = generate_empty_matrix
+    fill_matrix(empty_matrix)
+  end
+
+  def fill_matrix(matrix)
+    @edge_list.each do |edge|
+      first_index = edge[0].id
+      second_index = edge[1].id
+      edge_length = edge[2]
+      matrix[first_index][second_index] = edge_length
+    end
+    matrix
+  end
+
+  def render
+    @matrix.each do |row|
+      p row
+    end
   end
 
   def generate_empty_matrix
@@ -19,7 +36,14 @@ class AdjacencyMatrix
   end
 
   def vertices
-
+    vertices = @edge_list.flatten.uniq
+    vertices.select! {|element| element.is_a?(Person)}
   end
 
+  def sorted_vertices
+    vertices.sort_by {|element| element.id} 
+  end
 end
+
+a = AdjacencyMatrix.new
+a.render
