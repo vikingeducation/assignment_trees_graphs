124d9f688b208443a0939aacb25e64b82ccb79d5
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-09 12:35:35 -0600

    working on build

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -1,14 +1,35 @@
 Node = Struct.new(:num, :left, :right)
 
 class BinaryTree
-  attr_reader :root
-  def initialize(arr)
-    @root = Node.new(arr[0])
 
+  attr_reader :root
+
+  def initialize(arr)
+    build(arr)
   end
 
   def build(arr)
-    arr.each_with_index
+    # sets root as first element
+    # loop through rest of array
+    # adding onto branches
+    # until looped through array
+
+    @root = Node.new(arr[0])
+
+    arr[1..-1].each do |node|
+    end
+
   end
+
+  def add(node, root)
+    branch = node.num > root.num ? root.right : root.left
+    if branch
+      add(node, branch)
+    else
+      branch = node
+    end
+  end
+
+
 
 end
--- spec/binary_tree_spec.rb
+++ spec/binary_tree_spec.rb
@@ -3,6 +3,7 @@ require 'binary_tree'
 
 describe BinaryTree do
   let(:tree) {BinaryTree.new([5,7,6,2,9,8])}
+
   describe "#new" do
     it "takes an array" do
       expect{tree}.not_to raise_error
@@ -13,8 +14,11 @@ describe BinaryTree do
     it "sets the first element to be the tree root" do
       expect(tree.root.num).to eq(5)
     end
-    it "elements larger than the root ar place in the right branch"
+    it "elements larger than the root ar place in the right branch" do
       expect(tree.root.right.num).to eq(7)
     end
   end
+
+  
+
 end
