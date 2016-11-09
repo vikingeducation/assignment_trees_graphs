023e40cb9d49183ac01893f8cd85f998f01ca322
Alexa <alexa.anderson@outlook.com>
2016-11-09 14:02:03 -0600

    Edge lists and rspec

---
+++ lib/adjacency_matrix.rb
@@ -0,0 +1,27 @@
+class AdjacencyMatrix
+
+  def initialize(edge_list)
+    @edge_list = edge_list
+  end
+
+  def matrix(edge_list)
+    #initial ize amtrix []
+    edge_list.each do |edge|
+      #get orgin, get destination, get weight
+      # set matrix[origin][destin] = weight
+    end
+  end
+
+  def print_matrix
+    # outputs as a matrix of names with an X
+    # if there is no edge 
+  end
+
+  def edge_weight
+
+  end
+
+
+end
+
+# am = AdjacencyMatrix.new( EDGE_LIST )
---
+++ spec/adjacency_matrix_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require 'adjacency_matrix'
+require 'person'
+
+describe AdjacencyMatrix do
+  # let(:a) { AdjacencyMatrix.new }
+  let(:el) { 
+    [
+      [Person.new(1 ,'Harry'), Person.new(2 ,'Alice'), 2],
+      [Person.new(1 ,'Harry'), Person.new(0 ,'Bob'), 9],
+      [Person.new(2 ,'Alice'), Person.new(0 ,'Bob'), 11]
+    ]
+  }
+  let(matrix) {
+    [
+      [nil, nil, nil],
+      [9,   nil,   2],
+      [11,  nil, nil]
+    ]
+  }
+
+  describe '#new' do
+    it "accepts an edge list" do
+      expect{AdjacencyMatrix.new(el)}.not_to raise_error
+    end
+
+    it "converts the edge list into a matrix" do
+      am = AdjacencyMatrix.new(el)
+      (am.matrix).to eq(matrix)
+    end
+  end
+end
