5e71c884dcb77bf2869a2f08153f80d667b3a186
David Wiesenberg <david_britta@yahoo.com>
2017-01-24 16:50:23 -0500

    final

---
+++ test.rb
@@ -0,0 +1,15 @@
+# test
+
+array_1 = Array.new(3) {Array.new(5)}
+
+
+array_1[2][2] = 6
+
+
+puts "array_1 = #{array_1}"
+
+
+# puts "array_1[6] = #{array_1[6]}"
+
+
+# puts "array_1[3][1] = #{array_1[3][1]}"
