46301658d701cdc9b379234419486b35f8926d4d
Chad Lucas <chad.lucas.19@gmail.com>
2017-03-31 16:11:44 -0500

    add build_tree method

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -5,13 +5,29 @@ class BinaryTree
   attr_accessor :root
 
   def initialize(array)
+  	@array = array
 	  #sets root node with first element in array
 	  @root = BinaryNode.new(nil, nil, array[0])
   end
 
+  def build_tree
+  	#iterate though array and check if goes to left or 
+  	#right of root
+    @array.each do |x|
+      while x < @root.data
+        @left_node = BinaryNode.new(:left, :right, :data)
+        @left_node.data = x
+        puts @left_node.data
+      end
+    end
+  puts   @left_node.data.inspect
+  end
+
 end
 
-tree = BinaryTree.new([15,1,4,18,6,3,8])
 
-puts tree.root.data
+binary_tree = BinaryTree.new([8,10,3,1,6,14,4,7,13])
+
+binary_tree.build_tree
+
 
