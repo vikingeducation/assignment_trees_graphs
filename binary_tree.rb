3f5bc24c86f916b27f33d0119a949bbdc062ac25
Roy Chen <roychenlei@gmail.com>
2017-04-02 13:34:08 +0800

    fixed #add method

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,13 +1,37 @@
 class BinaryTree
-  Node = Struct.new(:data, :left, :right)
+  Node = Struct.new(:value, :left, :right)
+
+  attr_reader :root
 
   def initialize
     @root = nil
   end
 
-  # inserts an array of values into the BinaryTree.
-  # assumes the input is an array of non-repeating integers.
-  def insert(values)
+  # adds a value into the BinaryTree
+  def add(value)
+    if @root.nil?
+      @root = Node.new(value, nil, nil)
+      return @root
+    end
+
+    current_node = @root
+    while (current_node)
+      if value <= current_node.value
+        if current_node.left.nil?
+          current_node.left = Node.new(value, nil, nil)
+          return current_node.left
+        else
+          current_node = current_node.left
+        end
+      else
+        if current_node.right.nil?
+          current_node.right = Node.new(value, nil, nil)
+          return current_node.right
+        else
+          current_node = current_node.right
+        end
+      end
+    end
   end
 
   # prints out the BinaryTree
