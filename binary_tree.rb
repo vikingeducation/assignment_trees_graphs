fbcf387399e80bf8135966ee6274dba595259f57
Roy Chen <roychenlei@gmail.com>
2017-04-02 11:09:31 +0800

    initial class/method definition

---
+++ binary_tree.rb
@@ -0,0 +1,16 @@
+class BinaryTree
+  Node = Struct.new(:data, :left, :right)
+
+  def initialize
+    @root = nil
+  end
+
+  # inserts an array of values into the BinaryTree.
+  # assumes the input is an array of non-repeating integers.
+  def insert(values)
+  end
+
+  # prints out the BinaryTree
+  def render
+  end
+end
