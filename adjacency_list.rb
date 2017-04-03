1360a735e57fac015a5d01d048dc09e06868060a
Roy Chen <roychenlei@gmail.com>
2017-04-03 20:24:55 +0800

    added code to build lookup table upon initialization

--- adjacency_list.rb
+++ adjacency_list.rb
@@ -1,9 +1,38 @@
 require_relative './linked_list'
+require_relative './edge_list'
 
 class AdjacencyList
-  def initialize; end
+  def initialize(edge_list = nil)
+    @lookup = nil
+
+    unless edge_list.nil?
+      @lookup = build_lookup(edge_list)
+    end
+  end
 
   def print_adj_list; end
 
   def edge_weight(from, to); end
+
+  private
+
+  def build_lookup(edge_list)
+    from_hash = {}
+    to_hash = {}
+
+    edge_list.each do |edge|
+      from = edge[0]
+      to = edge[1]
+
+      from_hash[from.id] = from.name
+      to_hash[to.id] = to.name
+    end
+
+    @lookup = from_hash.merge(to_hash)
+  end
+end
+
+if __FILE__ == $0
+  al = AdjacencyList.new(EDGE_LIST)
+  p al
 end
