8287101f729773fb8f5ec0441a1058c43999cb2e
Roy Chen <roychenlei@gmail.com>
2017-04-03 21:28:45 +0800

    renamed @contents to @buckets, implemented #print_adj_list

--- adjacency_list.rb
+++ adjacency_list.rb
@@ -3,19 +3,33 @@ require_relative './edge_list'
 
 class AdjacencyList
   attr_reader :lookup,
-              :contents
+              :buckets
 
   def initialize(edge_list = nil)
     @lookup = nil
-    @contents = []
+    @buckets = []
 
     unless edge_list.nil?
       @lookup = build_lookup(edge_list)
-      @contents = build_adj_list(edge_list)
+      @buckets = build_adj_list(edge_list)
     end
   end
 
-  def print_adj_list; end
+  def print_adj_list
+    self.buckets.length.times do |index|
+      unless self.buckets[index].nil?
+        bucket_contents = ""
+
+        # add source vertex name
+        bucket_contents << "#{self.lookup[index]}    "
+
+        # now iterate through the linked list and read the destination vertices
+        bucket_contents << read_adj_list_bucket(self.buckets[index].head)
+
+        puts bucket_contents
+      end
+    end
+  end
 
   def edge_weight(from, to); end
 
@@ -37,22 +51,36 @@ class AdjacencyList
   end
 
   def build_adj_list(edge_list)
-    contents = []
+    buckets = []
 
     edge_list.each do |edge|
       from = edge[0].id
       to = edge[1].id
       weight = edge[2]
 
-      contents[from] = LinkedList.new if contents[from].nil?
+      buckets[from] = LinkedList.new if buckets[from].nil?
 
-      contents[from].insert_at_end(to, weight)
+      buckets[from].insert_at_end(to, weight)
     end
 
-    contents
+    buckets
+  end
+
+  # NOTE: I know, I know, DRY.. it's easier to just write this method
+  # to read each bucket, rather than modify #LinkedList#read
+  def read_adj_list_bucket(node)
+    output = ""
+    while (node)
+      output << "#{self.lookup[node.to]}(#{node.weight}), "
+      node = node.next_node
+    end
+
+    # remove trailing space and comma
+    output[0..-3]
   end
 end
 
 if __FILE__ == $0
   al = AdjacencyList.new(EDGE_LIST)
+  al.print_adj_list
 end
