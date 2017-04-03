9f45804e85f49857ae36a689fe75820e285883bd
Roy Chen <roychenlei@gmail.com>
2017-04-03 13:45:41 +0800

    implemented method to build lookup table

--- adjacency_matrix.rb
+++ adjacency_matrix.rb
@@ -1,15 +1,19 @@
 require_relative './edge_list'
 
 class AdjacencyMatrix
-  attr_reader :matrix
+  attr_reader :matrix,
+              :lookup
 
   def initialize(edge_list = nil)
     @matrix = []
+    @lookup = {}
 
     build_matrix(edge_list) unless edge_list.nil?
+    build_lookup(edge_list) unless edge_list.nil?
   end
 
   def print_matrix
+
   end
 
   def edge_weight(from, to)
@@ -27,9 +31,24 @@ class AdjacencyMatrix
       @matrix[from.id][to.id] = weight
     end
   end
+
+  def build_lookup(edge_list)
+    from_hash = {}
+    to_hash = {}
+
+    edge_list.each do |edge|
+      from = edge[0]
+      to = edge[1]
+
+      from_hash[from.id] = from.name
+      to_hash[to.id] = to.name
+    end
+
+    @lookup = from_hash.merge(to_hash)
+  end
 end
 
 if __FILE__ == $0
   am = AdjacencyMatrix.new(EDGE_LIST)
-  p am
+  p am.lookup
 end
