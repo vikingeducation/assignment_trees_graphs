a6b3b8e78c030d56d775bd4a4bf7fd6c5498e5ee
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-12 16:22:15 -0500

    add left_node method

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,33 +1,60 @@
 #node struct with left right children
-BinaryNode = Struct.new(:left, :right, :data)
+Node = Struct.new(:left, :right, :data)
 
 class BinaryTree
-  attr_accessor :root
+  attr_accessor :root, :left_node, :right_node
 
   def initialize(array)
   	@array = array
 	  #sets root node with first element in array
-	  @root = BinaryNode.new(nil, nil, array[0])
+	  @root = Node.new(nil, nil, array[0])
+    @current = @root
   end
 
   def build_tree
-  	#iterate though array and check if goes to left or 
-  	#right of root
-    @array.each do |x|
-      while x < @root.data
-        @left_node = BinaryNode.new(:left, :right, :data)
-        @left_node.data = x
-        puts @left_node.data
+  	#iterate though array
+    @array[1..-1].each do |data|
+      #check if data is less then root
+      if data < @root.data
+        #call method to add to left of tree
+       add_left_node(data)
+
+      elsif data > @root.data
+        
+        add_right_node(data)
       end
     end
-  puts   @left_node.data.inspect
   end
+
+  def add_left_node(data)
+    node = Node.new(nil, nil, data)
+    (@array.length-1).times do 
+      if node.data < @current.data && @current.left.nil?
+        @current.left = node
+        break
+      elsif node.data < @current.data && !@current.left.nil?
+        @current = @current.left
+      end
+    end
+        
+
+  end
+
+  def add_right_node(data)
+
+  end
+
+
 
 end
 
 
-binary_tree = BinaryTree.new([8,10,3,1,6,14,4,7,13])
 
+binary_tree = BinaryTree.new([8,10,3,1,6,14,4,7,13])
 binary_tree.build_tree
+
+puts binary_tree.inspect
+
+
 
 
