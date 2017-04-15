766a5eeeed06393a581844df90c35f691817536d
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-14 20:31:57 -0500

    add print method

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -20,15 +20,19 @@ class BinaryTree
        add_left_node(data)
 
       elsif data > @root.data
-        
+      #method to add data to right side of tree
         add_right_node(data)
       end
     end
+    puts @array.inspect
   end
 
   def add_left_node(data)
+
     node = Node.new(nil, nil, data)
+
     (@array.length-1).times do 
+      
       if node.data < @current.data && @current.left.nil?
         @current.left = node
         break
@@ -41,6 +45,15 @@ class BinaryTree
   end
 
   def add_right_node(data)
+    node = Node.new(nil,nil, data)
+    (@array.length-1).times do 
+      if node.data > @current.data && @current.right.nil?
+        @current.right = node
+        break
+      elsif node.data > @current.data && !@current.right.nil?
+        @current = @current.right
+      end
+    end
 
   end
 
@@ -52,8 +65,9 @@ end
 
 binary_tree = BinaryTree.new([8,10,3,1,6,14,4,7,13])
 binary_tree.build_tree
+puts 
 
-puts binary_tree.inspect
+#puts binary_tree.inspect
 
 
 
